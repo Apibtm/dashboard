7e977bfd04c53c05720f0c50b6ab675b7c1f33ef
my <my@my-PC>
2021-08-04 10:51:54 +0530

    This is my 1st Commit

---
+++ projects/src/test/java/com/ty/rmg/projects/CreateProjectTest.java
@@ -0,0 +1,11 @@
+package com.ty.rmg.projects;
+
+import org.testng.annotations.Test;
+
+public class CreateProjectTest {
+@Test
+public void createProjectTest()
+{
+	System.out.println("Hey i am here in Create project Test");
+	}
+}
---
+++ projects/src/test/java/com/ty/rmg/projects/UpdateProjectTest.java
@@ -0,0 +1,11 @@
+package com.ty.rmg.projects;
+
+import org.testng.annotations.Test;
+
+public class UpdateProjectTest {
+@Test
+public void updateProjectTest()
+{
+	System.out.println("Hey i am there in update project Test");
+	}
+}
